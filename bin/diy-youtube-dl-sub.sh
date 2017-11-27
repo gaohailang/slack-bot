97cc0cb053cbac33054553aef4abd90bcf576ea0
Glitch (tame-bison) <none>
2017-11-27 15:30:49 +0000

    🐸🍹 Updated with Glitch

--- bin/diy-youtube-dl-sub.sh
+++ bin/diy-youtube-dl-sub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo $1
+echo $1;
 
-./youtube-dl --sub-format vtt --write-sub --write-auto-sub --skip-download $1
+./youtube-dl --sub-format vtt --write-sub --write-auto-sub --skip-download $1;
 
-cat *.vtt|sed -E "s;<[0-9]{2}:[^>]*?>;;g"|sed -E "s;</?c(\.color[^>]*?)?>;;g" | sed -E "s;[0-9]{2}:[0-9]{2}:.*?\s-->\s[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}(.*?position:[0-9]+?%)?;;g" | tr '\n' ' '|sed -E 's;\s{2,}; ;g'|sed -E 's;WEBVTT.*##;;'
+cat *.vtt|sed -E "s;<[0-9]{2}:[^>]*?>;;g"|sed -E "s;</?c(\.color[^>]*?)?>;;g" | sed -E "s;[0-9]{2}:[0-9]{2}:.*?\s-->\s[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}(.*?position:[0-9]+?%)?;;g" | tr '\n' ' '|sed -E 's;\s{2,}; ;g'|sed -E 's;WEBVTT.*##;;';
 
-rm -f *.vtt
+rm -f *.vtt;
